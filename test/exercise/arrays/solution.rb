36d967def29439642062c0f5bffd559a9a02c919
Lyubov Parfilova <cnlmode@gmail.com>
2021-01-01 17:14:54 +0400

    add replace() function implementation

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,8 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        replacement = array.max
+        array.map { |num| num.positive? ? replacement : num }
       end
 
       def search(_array, _query)
