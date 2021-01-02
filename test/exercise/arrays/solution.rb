f482d3c3ca201490413721253f301c2a09a0d2a2
Lyubov Parfilova <cnlmode@gmail.com>
2021-01-02 10:52:58 +0400

    add search() function implementation

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -6,8 +6,30 @@ module Exercise
         array.map { |num| num.positive? ? replacement : num }
       end
 
-      def search(_array, _query)
-        0
+      def search(haystack, needle)
+        not_found = -1
+
+        return not_found if haystack.empty?
+
+        binary_search = lambda { |start_key, end_key|
+          return not_found if end_key < start_key
+
+          if start_key == end_key
+            result = haystack[start_key] == needle ? start_key : not_found
+            return result
+          end
+
+          middle_key = (start_key + end_key) / 2
+
+          return middle_key if haystack[middle_key] == needle
+
+          if haystack[middle_key] > needle
+            binary_search.call(start_key, middle_key - 1)
+          else
+            binary_search.call(middle_key + 1, end_key)
+          end
+        }
+        binary_search.call(0, haystack.length - 1)
       end
     end
   end
